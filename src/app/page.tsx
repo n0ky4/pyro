37e92d09cc5e2729dd0f1a875f01eeaaaaf6a6c4
nokya <n0ky4@proton.me>
2023-07-11 18:03:12 -0300

    Add palletes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Button from '@/components/Button'
 import ColorCard from '@/components/FeaturedColor/ColorCard'
 import FeaturedColorInfo from '@/components/FeaturedColor/FeaturedColorInfo'
+import Pallete from '@/components/Pallete'
 import { getRandomColor } from '@/util/color'
 import { Palette, Shuffle } from './../assets/icons'
 
@@ -8,7 +9,7 @@ export default async function Home() {
     const data = await getRandomColor()
 
     return (
-        <main>
+        <main className='mb-48'>
             <div className='w-full max-w-screen-lg mx-auto px-4'>
                 <div className='py-6 border-b-4 border-slate-200 flex items-center justify-between mb-8'>
                     <h1 className='text-4xl font-bold'>🔥 pyro</h1>
@@ -30,6 +31,14 @@ export default async function Home() {
                     </div>
                     <ColorCard data={data} />
                     <FeaturedColorInfo data={data} />
+                    <div className='flex flex-col gap-2'>
+                        <h1 className='text-2xl font-bold'>Tons escuros</h1>
+                        <Pallete colors={data.shades} linkColors />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <h1 className='text-2xl font-bold'>Tons claros</h1>
+                        <Pallete colors={data.tints} linkColors />
+                    </div>
                 </div>
             </div>
         </main>
