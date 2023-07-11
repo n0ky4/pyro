cced8d5b6a998e6210160b4dfcae12e94bb205c9
nokya <n0ky4@proton.me>
2023-07-11 19:46:25 -0300

    Improve color formatting

--- src/util/colorFormat.ts
+++ src/util/colorFormat.ts
@@ -1,24 +1,26 @@
 import { Hsl, Hsv, Rgb } from 'culori'
 
-export function formatRGB(rgb: Rgb) {
-    const red = Math.round(rgb.r * 255)
-    const green = Math.round(rgb.b * 255)
-    const blue = Math.round(rgb.b * 255)
+export function formatRGB(rgb: Rgb | undefined) {
+    const red = Math.round((rgb?.r || 0) * 255)
+    const green = Math.round((rgb?.g || 0) * 255)
+    const blue = Math.round((rgb?.b || 0) * 255)
     return [red, green, blue]
 }
 
-export function formatHSL(hsl: Hsl) {
-    const hue = Math.round(hsl.h || 1) + '°'
-    const saturation = Math.round(hsl.s * 100) + '%'
-    const lightness = Math.round(hsl.l * 100) + '%'
-    return [hue, saturation, lightness]
+export function formatHSL(hsl: Hsl | undefined, numbers?: boolean) {
+    const hue = Math.round(hsl?.h || 0)
+    const saturation = Math.round((hsl?.s || 0) * 100)
+    const lightness = Math.round((hsl?.l || 0) * 100)
+    if (numbers) return [hue, saturation, lightness]
+    return [hue + '°', saturation + '%', lightness + '%']
 }
 
-export function formatHSV(hsv: Hsv) {
-    const hue = Math.round(hsv.h || 1) + '°'
-    const saturation = Math.round(hsv.s * 100) + '%'
-    const value = Math.round(hsv.v * 100) + '%'
-    return [hue, saturation, value]
+export function formatHSV(hsv: Hsv | undefined, numbers?: boolean) {
+    const hue = Math.round(hsv?.h || 0)
+    const saturation = Math.round((hsv?.s || 0) * 100)
+    const value = Math.round((hsv?.v || 0) * 100)
+    if (numbers) return [hue, saturation, value]
+    return [hue + '°', saturation + '%', value + '%']
 }
 
 export function removeHash(hex: string) {
