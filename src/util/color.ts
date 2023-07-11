3096a809f605f4438d1d35fd118332ead2ba98e0
nokya <n0ky4@proton.me>
2023-07-11 18:02:15 -0300

    Add a bunch of functions

--- src/util/color.ts
+++ src/util/color.ts
@@ -1,6 +1,18 @@
-import { Hsl, Hsv, Rgb, differenceCiede2000, hsl, hsv, nearest, rgb } from 'culori'
+import {
+    Color,
+    Hsl,
+    Hsv,
+    Rgb,
+    differenceCiede2000,
+    formatHex,
+    hsl,
+    hsv,
+    nearest,
+    rgb,
+} from 'culori'
 import fs from 'fs'
 import path from 'path'
+import { addHash, removeHash } from './colorFormat'
 import { getTodayDate } from './date'
 import { createCacheFile, getCacheFile } from './file'
 
@@ -8,22 +20,25 @@ export interface ColorNames {
     [hex: string]: string
 }
 
+export interface Cmyk {
+    mode: 'cmyk'
+    c: number
+    m: number
+    y: number
+    k: number
+    alpha?: number
+}
+
 export interface ColorInfo {
+    hex: string
     name: string
     nearestNamedColor: string
-    hex: string
+    shades: string[]
+    tints: string[]
     rgb: Rgb | undefined
     hsl: Hsl | undefined
     hsv: Hsv | undefined
-    totalExecTime?: number
-    getColorNamesExecTime?: number
-    colorObjectKeysExecTime?: number
-    returnNamedColorExecTime?: number
-}
-
-export interface DailyColorData {
-    info: ColorInfo
-    refreshAt: number
+    cmyk: Cmyk | undefined
 }
 
 export const COLORNAMES_PATH = path.resolve('src/assets/data/colornames.min.json')
@@ -57,8 +72,32 @@ export function randomHexColor(length: number): string[] {
     return colors
 }
 
+export function getCmyk(hex: string): Cmyk {
+    hex = removeHash(hex)
+
+    const RGB = rgb(hex)
+    const red = RGB?.r || 255
+    const green = RGB?.g || 255
+    const blue = RGB?.b || 255
+
+    const max = Math.max(red, green, blue)
+
+    const cyan = (max - red) / max
+    const magenta = (max - green) / max
+    const yellow = (max - blue) / max
+    const black = 1 - max
+
+    return {
+        mode: 'cmyk',
+        c: Math.round(cyan * 100),
+        m: Math.round(magenta * 100),
+        y: Math.round(yellow * 100),
+        k: Math.round(black * 100),
+    }
+}
+
 export function getColorInfo(hex: string): ColorInfo {
-    if (hex.startsWith('#')) hex.slice(1)
+    hex = addHash(hex)
 
     const rawColors = getColorNames()
     const colors = Object.keys(rawColors)
@@ -67,12 +106,15 @@ export function getColorInfo(hex: string): ColorInfo {
     const cNearest = nearestNamedColor(hex, 1)[0]
 
     const colorInfo: ColorInfo = {
-        name: rawColors[cNearest],
-        nearestNamedColor: cNearest,
         hex: hex.toLowerCase(),
+        name: rawColors[cNearest],
+        nearestNamedColor: addHash(cNearest),
+        shades: colorShades(hex),
+        tints: colorTints(hex),
         rgb: rgb(hex),
         hsl: hsl(hex),
         hsv: hsv(hex),
+        cmyk: getCmyk(hex),
     }
 
     return colorInfo
@@ -83,19 +125,14 @@ interface CachedFile {
     generatedAt: string
 }
 
-export function getDailyColor(): DailyColorData {
+export function getDailyColor(): ColorInfo {
     const date = getTodayDate()
     const unixTs = Math.floor(date.getTime() / 1000)
-
     const cachedFile = getCacheFile(`daily-color-${unixTs}.json`)
-    const refreshAt = unixTs + 86400
 
     if (fs.existsSync(cachedFile)) {
         const { info } = JSON.parse(fs.readFileSync(cachedFile, 'utf-8')) as CachedFile
-        return {
-            info,
-            refreshAt,
-        }
+        return info
     }
 
     const colorNames = Object.entries(getColorNames())
@@ -115,10 +152,7 @@ export function getDailyColor(): DailyColorData {
         console.log(err)
     }
 
-    return {
-        info: data.info,
-        refreshAt,
-    }
+    return data.info
 }
 
 // Generates a random named color
@@ -128,3 +162,89 @@ export function getRandomColor(): ColorInfo {
     const [hex] = colorNames[rndIndex]
     return getColorInfo(hex)
 }
+
+export function colorShades(color: string) {
+    const length = 10
+    const HSV = hsv(color)
+    let res = []
+
+    const V = HSV?.v || 0
+    const toSubtract = V / length
+
+    for (let i = 0; i <= length; i++) {
+        const newHSV = { ...HSV, v: V - toSubtract * i }
+        const newHex = formatHex(newHSV as Color)
+        res.push(newHex)
+    }
+
+    return res
+}
+
+export function colorTints(color: string) {
+    const length = 10
+    const HSV = hsv(color)
+    let res = []
+
+    const S = HSV?.s || 0
+    const V = HSV?.v || 0
+    const subtractS = S / length
+    const addV = (1 - V) / length
+
+    for (let i = 0; i <= length; i++) {
+        const newHSV = { ...HSV, s: S - subtractS * i, v: V + addV * i }
+        const newHex = formatHex(newHSV as Color)
+        res.push(newHex)
+    }
+
+    return res
+}
+
+export function colorHues(color: string) {
+    const length = 10
+    const HSV = hsv(color)
+    let res = []
+
+    const H = HSV?.h || 0
+    const toAdd = 360 / length
+
+    for (let i = 0; i <= length; i++) {
+        const newHSV = { ...HSV, h: H + toAdd * i }
+        const newHex = formatHex(newHSV as Color)
+        res.push(newHex)
+    }
+
+    return res
+}
+
+// function test(type: 'hue' | 'shade' | 'tint', color: string) {
+//     const colors =
+//         type === 'hue'
+//             ? colorHues(color)
+//             : type === 'shade'
+//             ? colorShades(color)
+//             : colorTints(color)
+
+//     const strings = []
+//     for (const color of colors) {
+//         strings.push(chalk.bgHex(color).hex(color)('  '))
+//     }
+//     console.log(strings.join(''))
+// }
+
+// function logInfo(color: ColorInfo | string) {
+//     const blocks = '██████████████████████'
+//     if (typeof color === 'string') {
+//         console.log(`${chalk.bgHex(color).hex(color)(blocks)}`)
+//         return
+//     }
+//     console.log(`${chalk.bgHex(color.hex).hex(color.hex)(blocks)} ${color.name}`)
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const rnd = randomHexColor(1)[0]
+//     logInfo(rnd)
+//     test('hue', rnd)
+//     test('shade', rnd)
+//     test('tint', rnd)
+//     console.log()
+// }
