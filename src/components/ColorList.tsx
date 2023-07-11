7e99e90955a50f3aaad5030427978ed11ab274fe
nokya <n0ky4@proton.me>
2023-07-11 20:10:18 -0300

    Add ColorList component

---
+++ src/components/ColorList.tsx
@@ -0,0 +1,21 @@
+import { removeHash } from '@/util/colorFormat'
+import Link from 'next/link'
+
+interface ColorListProps {
+    colors: string[]
+}
+
+export default function ColorList({ colors }: ColorListProps) {
+    return (
+        <div className='flex flex-wrap items-center justify-between gap-2'>
+            {colors.map((color, index) => (
+                <Link
+                    href={`/${removeHash(color)}`}
+                    key={index}
+                    className='w-[86px] h-[86px] rounded-lg hover:opacity-75 transition-opacity'
+                    style={{ backgroundColor: color }}
+                />
+            ))}
+        </div>
+    )
+}
