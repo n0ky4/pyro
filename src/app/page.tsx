78d886b75ee43ab9ff54cd6c6f7a9b24d0437b82
nokya <n0ky4@proton.me>
2023-07-12 23:45:43 -0300

    Add metadata & navbar title link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,73 @@
 import Button from '@/components/Button'
+import ColorFavicon from '@/components/ColorFavicon'
 import ColorList from '@/components/ColorList'
 import ColorCard from '@/components/FeaturedColor/ColorCard'
 import FeaturedColorInfo from '@/components/FeaturedColor/FeaturedColorInfo'
 import Pallete from '@/components/Pallete'
-import { getRandomColor } from '@/util/color'
+import { ColorInfo, getRandomColor } from '@/util/color'
+import { Metadata } from 'next'
+import Link from 'next/link'
 import { Palette, Shuffle } from './../assets/icons'
 
+let data: ColorInfo | null = null
+
+export async function generateMetadata(): Promise<Metadata> {
+    if (!data) return {}
+    return {
+        title: `pyro - ${data.hex}`,
+        themeColor: data.hex,
+    }
+}
+
 export default async function Home() {
-    const data = await getRandomColor()
+    data = await getRandomColor()
 
     return (
-        <main className='mb-48'>
-            <div className='w-full max-w-screen-lg mx-auto px-4'>
-                <div className='py-6 border-b-4 border-slate-200 flex items-center justify-between mb-8'>
-                    <h1 className='text-4xl font-bold'>🔥 pyro</h1>
-                    <div className='flex items-center gap-4'>
-                        <Button ghost>
-                            <Palette size={22} weight='bold' />
-                            Paletas
-                        </Button>
-                        <Button>
-                            <Shuffle size={22} weight='bold' />
-                            Cor aleatória
-                        </Button>
+        <>
+            <ColorFavicon color={data.hex} />
+            <main className='mb-48'>
+                <div className='w-full max-w-screen-lg mx-auto px-4'>
+                    <div className='py-6 border-b-4 border-slate-200 flex items-center justify-between mb-8'>
+                        <Link href='/'>
+                            <h1 className='text-4xl font-bold'>🔥 pyro</h1>
+                        </Link>
+                        <div className='flex items-center gap-4'>
+                            <Button ghost>
+                                <Palette size={22} weight='bold' />
+                                Paletas
+                            </Button>
+                            <Button>
+                                <Shuffle size={22} weight='bold' />
+                                Cor aleatória
+                            </Button>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-8'>
+                        <div className='flex items-center gap-4'>
+                            <h1 className='text-6xl font-bold'>Cor destaque</h1>
+                            <p className='text-slate-400'>reseta em 2 horas</p>
+                        </div>
+                        <ColorCard data={data} />
+                        <FeaturedColorInfo data={data} />
+                        <div className='flex flex-col gap-2'>
+                            <h1 className='text-2xl font-bold'>Tons escuros</h1>
+                            <Pallete colors={data.shades} linkColors />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <h1 className='text-2xl font-bold'>Tons claros</h1>
+                            <Pallete colors={data.tints} linkColors />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <h1 className='text-2xl font-bold'>Hue</h1>
+                            <Pallete colors={data.hues} linkColors />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <h1 className='text-2xl font-bold'>Cores relacionadas</h1>
+                            <ColorList colors={data.related} />
+                        </div>
                     </div>
                 </div>
-                <div className='flex flex-col gap-8'>
-                    <div className='flex items-center gap-4'>
-                        <h1 className='text-6xl font-bold'>Cor destaque</h1>
-                        <p className='text-slate-400'>reseta em 2 horas</p>
-                    </div>
-                    <ColorCard data={data} />
-                    <FeaturedColorInfo data={data} />
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-2xl font-bold'>Tons escuros</h1>
-                        <Pallete colors={data.shades} linkColors />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-2xl font-bold'>Tons claros</h1>
-                        <Pallete colors={data.tints} linkColors />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-2xl font-bold'>Hue</h1>
-                        <Pallete colors={data.hues} linkColors />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-2xl font-bold'>Cores relacionadas</h1>
-                        <ColorList colors={data.related} />
-                    </div>
-                </div>
-            </div>
-        </main>
+            </main>
+        </>
     )
 }
