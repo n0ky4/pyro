99e7d7125b231a14f96b8a89563d647dd7276544
nokya <n0ky4@proton.me>
2023-07-15 18:18:51 -0300

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,38 @@
+import Button from '@/components/Button'
+import ColorLink from '@/components/ColorLink'
+import NavBar from '@/components/NavBar'
+import { getRandomColor } from '@/util/color'
+import Link from 'next/link'
+
+export default function Page404() {
+    const rndColor = getRandomColor()
+
+    return (
+        <main className='mb-48'>
+            <div className='w-full max-w-screen-lg mx-auto px-4'>
+                <NavBar />
+                <div className='w-full max-w-screen-sm mx-auto flex flex-col gap-8'>
+                    <div className='flex flex-col gap-10'>
+                        <h1 className='text-8xl font-bold'>Ops...</h1>
+                        <div className='flex flex-col gap-4 text-slate-800 text-lg text-justify'>
+                            <p>
+                                Você encontrou uma cor tão única que está além do espectro visível
+                                de cores! Infelizmente nossos olhos ainda não conseguem enxergá-la
+                                😓
+                            </p>
+                            <p>
+                                Que tal explorar outras cores? Essa aqui, por exemplo, se chama{' '}
+                                <ColorLink info={rndColor} /> e é bem bonita!
+                            </p>
+                        </div>
+                        <div className='w-full flex items-center justify-center'>
+                            <Link href='/'>
+                                <Button>Voltar para a página inicial</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ src/components/ColorLink.tsx
@@ -0,0 +1,41 @@
+import { ColorInfo } from '@/util/color'
+import { removeHash } from '@/util/colorFormat'
+import Link from 'next/link'
+
+interface ColorLinkProps {
+    info: ColorInfo
+    center?: boolean
+}
+
+export default function ColorLink({ info, center }: ColorLinkProps) {
+    const svgSize = 16
+    const svg = (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={svgSize}
+            height={svgSize}
+            viewBox={`0 0 ${svgSize} ${svgSize}`}
+        >
+            <rect width='100%' height='100%' fill={info.hex} rx='33.33%' />
+        </svg>
+    )
+
+    const component = (
+        <span className='inline-flex items-center gap-2 translate-y-[2px]'>
+            {svg}
+            <Link
+                href={`/${removeHash(info.hex)}`}
+                style={{ color: info.hex }}
+                className='underline'
+            >
+                {info.name}
+            </Link>
+        </span>
+    )
+
+    if (center) {
+        return <div className='w-full flex items-center justify-center'>{component}</div>
+    }
+
+    return component
+}
