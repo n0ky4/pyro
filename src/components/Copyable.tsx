fc8c9b44ba7dd1db6af3290006f8ef8584a82f98
nokya <n0ky4@proton.me>
2023-07-15 20:14:11 -0300

    Remove deprecated clipboard api

--- src/components/Copyable.tsx
+++ src/components/Copyable.tsx
@@ -1,5 +1,4 @@
 import { Copy } from '@/assets/icons'
-import { copy } from '@/util/clipboard'
 
 type CopyableProps = {
     value: string
@@ -10,16 +9,22 @@ type CopyableProps = {
 const icon = <Copy size={18} weight='fill' />
 
 export default function Copyable({ value, label = value, side = 'left' }: CopyableProps) {
-    const doCopy = async () => {
-        const success = await copy(value)
-        if (success) return alert('✅ Copiado com sucesso.')
-        alert('Não foi possível copiar a cor.')
+    const copy = () => {
+        navigator.clipboard
+            .writeText(value)
+            .then(() => {
+                return alert('✅ Copiado com sucesso.')
+            })
+            .catch((err) => {
+                console.log(err)
+                alert('Não foi possível copiar a cor.')
+            })
     }
 
     return (
         <button
             className='inline-flex items-center gap-2 hover:opacity-75 transition-opacity'
-            onClick={() => doCopy()}
+            onClick={() => copy()}
         >
             {side === 'left' && icon}
             {label}
