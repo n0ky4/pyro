8bef4bc4ae25428011c5860dca0b8029dfa1ee8f
nokya <n0ky4@proton.me>
2023-08-28 19:40:39 -0300

    test

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,8 +1,19 @@
-export default function ErrorPage() {
+export default async function ErrorPage() {
     const russianRoulette = Math.floor(Math.random() * 10)
     if (russianRoulette === 0) {
         throw new Error('Oh no! Something went wrong!')
     }
 
-    return <h1>yay</h1>
+    // prevent static rendering
+    const data = await fetch('https://fakerapi.it/api/v1/persons?_locale=pt_BR', {
+        method: 'GET',
+        cache: 'no-store',
+    })
+    const json = await data.json()
+
+    return (
+        <div>
+            <pre>{JSON.stringify(json, null, 2)}</pre>
+        </div>
+    )
 }
