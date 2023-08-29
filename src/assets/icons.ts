6b38d9cca7e816cb8ae3568fb4891a6db175b7f1
nokya <n0ky4@proton.me>
2023-08-29 12:35:34 -0300

    Export all icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,14 +1,3 @@
 'use client'
 
-import {
-    ArrowsClockwise,
-    CircleNotch,
-    Copy,
-    Heart,
-    List,
-    Palette,
-    Shuffle,
-    X,
-} from '@phosphor-icons/react'
-
-export { ArrowsClockwise, CircleNotch, Copy, Heart, List, Palette, Shuffle, X }
+export * from '@phosphor-icons/react'
