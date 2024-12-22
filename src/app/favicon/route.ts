d4850080e1291ced99bd6ff33bab3fdf36f0c603
nokya <n0ky4@proton.me>
2024-12-21 23:41:13 -0300

    [fix] use website color

--- src/app/favicon/route.ts
+++ src/app/favicon/route.ts
@@ -5,13 +5,15 @@ export const runtime = 'edge'
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url)
-    const hex = sanitizeColor(searchParams.get('hex') || 'ef4444')
+    const hex = sanitizeColor(searchParams.get('hex') || 'ff004d')
 
     const svgMarkup = `
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
             <rect width="100%" height="100%" fill="#${hex}" rx="33.33%"/>
         </svg>
     `
+        .trim()
+        .replace(/\s{2,}/g, '')
 
     return new NextResponse(svgMarkup, {
         headers: {
