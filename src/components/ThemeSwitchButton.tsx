fbaa0a2adbffabcfb67de21cb8d6a47bcc8b918a
nokya <n0ky4@proton.me>
2024-12-22 22:26:19 -0300

    [fix] dark mode icon flick

--- src/components/ThemeSwitchButton.tsx
+++ src/components/ThemeSwitchButton.tsx
@@ -2,14 +2,17 @@
 
 import { Moon, Sun } from '@phosphor-icons/react'
 import { useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 import Button from './Button'
 
 export function ThemeSwitchButton() {
     const [theme, setTheme] = useState('light')
+    const [mounted, setMounted] = useState(false)
 
     useEffect(() => {
         const isDark = document.documentElement.classList.contains('dark')
         setTheme(isDark ? 'dark' : 'light')
+        if (!mounted) setMounted(true)
     }, [])
 
     const toggle = () => {
@@ -18,7 +21,11 @@ export function ThemeSwitchButton() {
     }
 
     return (
-        <Button theme='ghost' onClick={() => toggle()}>
+        <Button
+            theme='ghost'
+            onClick={() => toggle()}
+            className={twMerge(mounted ? 'opacity-100' : 'opacity-0')}
+        >
             {theme === 'dark' ? <Sun size={22} weight='bold' /> : <Moon size={22} weight='bold' />}
         </Button>
     )
