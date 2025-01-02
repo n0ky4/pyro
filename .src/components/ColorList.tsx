e8986bffef32813401bb04eaf43807d9d1d347d1
nokya <n0ky4@proton.me>
2025-01-02 16:19:41 -0300

    [fix][temp] file casing

---
+++ .src/components/ColorList.tsx
@@ -0,0 +1,21 @@
+import { removeHash } from '@/util/colorFormat'
+import Link from 'next/link'
+
+interface ColorListProps {
+    colors: string[]
+}
+
+export default function ColorList({ colors }: ColorListProps) {
+    return (
+        <div className='grid gap-4 grid-cols-4 sm:grid-cols-5 md:grid-cols-9 lg:grid-cols-10'>
+            {colors.map((color, index) => (
+                <Link
+                    href={`/${removeHash(color)}`}
+                    key={index}
+                    className='w-20 h-20 md:w-20 md:h-20 rounded-xl hover:opacity-75 transition-opacity'
+                    style={{ backgroundColor: color }}
+                />
+            ))}
+        </div>
+    )
+}
