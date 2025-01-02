da4d0fffe817346c667745d579195ccc33207784
nokya <n0ky4@proton.me>
2025-01-02 16:51:31 -0300

    [fix] redirect

--- src/app/palette/route.ts
+++ src/app/palette/route.ts
@@ -1,6 +1,7 @@
 import getRandomPalette from '@/core/randomPaletteGenerator'
 import { removeHash } from '@/util/colorFormat'
-import { NextRequest, NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
 
 const getPaletteParam = () => {
     const { colors } = getRandomPalette()
@@ -8,13 +9,6 @@ const getPaletteParam = () => {
 }
 
 export async function GET(req: NextRequest) {
-    const url = req.nextUrl.clone()
-    url.pathname = `/palette/${getPaletteParam()}`
-
-    return new NextResponse(null, {
-        status: 302,
-        headers: {
-            Location: url.toString(),
-        },
-    })
+    const url = `/palette/${getPaletteParam()}`
+    return redirect(url)
 }
