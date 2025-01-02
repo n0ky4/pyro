5ddb8e2b713dfa2625031056a3c4223d240dd948
nokya <n0ky4@proton.me>
2025-01-02 16:48:38 -0300

    [fix] redirect not working in production

--- src/app/random/route.ts
+++ src/app/random/route.ts
@@ -1,5 +1,6 @@
 import color from '@/core/colorGenerator'
-import { NextRequest, NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -7,13 +8,6 @@ export const revalidate = 0
 export async function GET(req: NextRequest) {
     const hex = color.getRandomColor('named')
 
-    const url = req.nextUrl.clone()
-    url.pathname = `/${hex}`
-
-    return new NextResponse(null, {
-        status: 302,
-        headers: {
-            Location: url.toString() + '?r',
-        },
-    })
+    const url = `/${hex}?r`
+    return redirect(url)
 }
